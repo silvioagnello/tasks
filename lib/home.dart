afd38a6d628794602c27ed4075eb9f6c3b8f518b
silvioagnello <silvio.agnello@gmail.com>
2019-06-17 10:41:15 -0300

    com ID de categoria

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:task_mate/tasks.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,14 +12,42 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Category> _categories = [
-    Category("MERCADO", Icons.local_grocery_store, Colors.orange[100]),
-    Category("FARMÁCIA", Icons.local_pharmacy, Colors.pink[100]),
-    Category("CONSTRUÇÃO", Icons.build, Colors.green[100]),
-    Category("TAREFAS", Icons.style, Colors.blueAccent[100]),
-    Category("AUTOMÓVEL", Icons.directions_car, Colors.blue[400]),
-    Category("ROUPAS", Icons.card_giftcard, Colors.red[200]),
-    Category("OUTROS", Icons.dashboard, Colors.brown[200]),
+    Category("0", "MERCADO", Icons.local_grocery_store, Colors.orange[100]),
+    Category("1", "FARMÁCIA", Icons.local_pharmacy, Colors.pink[100]),
+    Category("2", "CONSTRUÇÃO", Icons.build, Colors.green[100]),
+    Category("3", "TAREFAS", Icons.style, Colors.blueAccent[100]),
+    Category("4", "AUTOMÓVEL", Icons.directions_car, Colors.blue[400]),
+    Category("5", "ROUPAS", Icons.card_giftcard, Colors.red[200]),
+    Category("6", "OUTROS", Icons.dashboard, Colors.brown[200]),
   ];
+
+  List _toDoList3 = [];
+  String qtPendCateg = '';
+  int qtPend0 = 0;
+  int qtPend1 = 0;
+  int qtPend2 = 0;
+  int qtPend3 = 0;
+  int qtPend4 = 0;
+  int qtPend5 = 0;
+  int qtPend6 = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _toDoList3 = json.decode(data);
+
+        qtPend0 = _toDoList3.where((c) => c["categ"] == "MERCADO").length;
+        qtPend1 = _toDoList3.where((c) => c["categ"] == "FARMÁCIA").length;
+        qtPend2 = _toDoList3.where((c) => c["categ"] == "CONSTRUÇÃO").length;
+        qtPend3 = _toDoList3.where((c) => c["categ"] == "TAREFAS").length;
+        qtPend4 = _toDoList3.where((c) => c["categ"] == "AUTOMÓVEL").length;
+        qtPend5 = _toDoList3.where((c) => c["categ"] == "ROUPAS").length;
+        qtPend6 = _toDoList3.where((c) => c["categ"] == "OUTROS").length;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,13 +75,40 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _categoryCards(context, idx) {
+    String qtPendCateg;
+    switch (_categories[idx].nmCateg) {
+      case "MERCADO":
+        qtPendCateg = qtPend0.toString();
+        break;
+      case "FARMÁCIA":
+        qtPendCateg = qtPend1.toString();
+        break;
+      case "CONSTRUÇÃO":
+        qtPendCateg = qtPend2.toString();
+        break;
+      case "TAREFAS":
+        qtPendCateg = qtPend3.toString();
+        break;
+      case "AUTOMÓVEL":
+        qtPendCateg = qtPend4.toString();
+        break;
+      case "ROUPAS":
+        qtPendCateg = qtPend5.toString();
+        break;
+      case "OUTROS":
+        qtPendCateg = qtPend6.toString();
+        break;
+      default:
+        qtPendCateg = '';
+    }
+
     return GestureDetector(
       onTap: () {
         Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => TasksPage(categ: _categories[idx].name.toString())),
-        );
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    TasksPage(idCateg: _categories[idx].idCateg.toString())));
       },
       child: Padding(
         padding:
@@ -68,6 +127,7 @@ class _HomePageState extends State<HomePage> {
             child: Padding(
               padding: const EdgeInsets.only(top: 20),
               child: ListTile(
+                trailing: Text("$qtPendCateg"),
                 leading: Container(
                   width: 70,
                   height: 70,
@@ -77,8 +137,8 @@ class _HomePageState extends State<HomePage> {
                       color: _categories[idx].color),
                   child: Icon(_categories[idx].icon, size: 40.0),
                 ),
-                title: Text(_categories[idx].name,
-                    style: TextStyle(fontSize: 28, color: Colors.black87)),
+                title: Text(_categories[idx].nmCateg,
+                    style: TextStyle(fontSize: 24, color: Colors.black87)),
               ),
             ),
           ),
@@ -89,9 +149,25 @@ class _HomePageState extends State<HomePage> {
 }
 
 class Category {
-  String name;
+  String idCateg;
+  String nmCateg;
   IconData icon;
   Color color;
 
-  Category(this.name, this.icon, this.color);
+  Category(this.idCateg, this.nmCateg, this.icon, this.color);
+}
+
+Future<File> _getFile() async {
+  final directory = await getApplicationDocumentsDirectory();
+  return File("${directory.path}/tasks.json");
+}
+
+Future<String> _readData() async {
+  try {
+    final file = await _getFile();
+    final s = file.lengthSync();
+    return file.readAsString();
+  } catch (e) {
+    return null;
+  }
 }
--- lib/tasks.dart
+++ lib/tasks.dart
@@ -6,18 +6,18 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class Categ {
-  String categ;
-  String name;
+  String idCateg;
+  String nmCateg;
+  String txtTarefa;
   bool ok;
 
-// Categ();
-  Categ(this.categ, this.name, this.ok);
+  Categ(this.idCateg, this.nmCateg, this.txtTarefa, this.ok);
 }
 
 class TasksPage extends StatefulWidget {
-  final String categ;
+  final String idCateg;
 
-  TasksPage({this.categ});
+  TasksPage({this.idCateg});
 
   @override
   _TasksPageState createState() => _TasksPageState();
@@ -28,10 +28,6 @@ class _TasksPageState extends State<TasksPage> {
 
   List _toDoList = [];
   List _toDoList2 = [];
-// List<Map<String, dynamic>> _toDoList = [
-  // {"name": "teste1", "ok": true},
-  // {"name": "teste2", "ok": false}
-// ];
 
   Map<String, dynamic> _lastRemoved;
   int _lastRemovedPos;
@@ -43,21 +39,52 @@ class _TasksPageState extends State<TasksPage> {
       setState(() {
         _toDoList = json.decode(data);
         _toDoList2 = json.decode(data);
-        _toDoList2.removeWhere((c) => c["categ"] == widget.categ);
-        _toDoList.removeWhere((c) => c["categ"] != widget.categ);
+        _toDoList2.removeWhere((c) => c["idCateg"] == widget.idCateg);
+        _toDoList.removeWhere((c) => c["idCateg"] != widget.idCateg);
       });
     });
   }
 
+String nmCateg(idCateg){
+  String nmCateg;
+   switch (idCateg) {
+      case "0":
+      nmCateg = 'MERCADO';
+        break;
+      case "1":
+      nmCateg = 'FARMÁCIA';
+        break;
+      case "2":
+      nmCateg = 'CONSTRUÇÃO';
+        break;
+      case "3":
+      nmCateg = 'TAREFAS';
+        break;
+      case "4":
+      nmCateg = 'AUTOMÓVEL';
+        break;
+      case "5":
+      nmCateg = 'ROUPAS';
+        break;
+      case "6":
+      nmCateg = 'OUTROS';
+        break;
+      default:
+      nmCateg = 'EXTRA';
+    }
+    return nmCateg;
+}
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newTask = {
-        "categ": "${widget.categ}",
-        "name": _toDoController.text,
+        "idCateg": "${widget.idCateg}",
+        "nmCateg": nmCateg(widget.idCateg), // "${widget.categ}",
+        "txtTarefa": _toDoController.text,
         "ok": false
       };
       _toDoController.text = "";
-      
+
       _toDoList.add(newTask);
       _saveData();
     });
@@ -84,7 +111,7 @@ class _TasksPageState extends State<TasksPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("${widget.categ}",
+          title: Text(nmCateg(widget.idCateg),
               style: TextStyle(
                   fontSize: 20.0,
                   // fontWeight: FontWeight.bold,
@@ -92,23 +119,36 @@ class _TasksPageState extends State<TasksPage> {
       body: Column(
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(12.0),
             child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  border: Border.all(color: Colors.grey[500], width: 1.0)),
               child: Row(
                 children: <Widget>[
                   Expanded(
-                      child: TextField(
-                          controller: _toDoController,
-                          // autofocus: true,
-                          style: TextStyle(fontSize: 20),
-                          decoration: InputDecoration(
-                              hintText: "Digite uma tarefa",
-                              labelStyle: TextStyle(color: Colors.blue)))),
-                  RaisedButton(
-                    elevation: 4.0,
-                    color: Colors.lightBlueAccent,
-                    child: Icon(Icons.check),
-                    onPressed: _addToDo,
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                        controller: _toDoController,
+                        // autofocus: true,
+                        style: TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                            hintText: "Digite uma tarefa",
+                            labelStyle: TextStyle(color: Colors.blue))),
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    // child: FlatButton(color: Colors.red, shape: ,
+                    //   child: Icon(Icons.check),
+                    //   onPressed: _addToDo,
+                    // ),
+                    child: RaisedButton(shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      elevation: 4.0,
+                      color: Colors.lightBlueAccent,
+                      child: Icon(Icons.check),
+                      onPressed: _addToDo,
+                    ),
                   ),
                 ],
               ),
@@ -141,7 +181,7 @@ class _TasksPageState extends State<TasksPage> {
               ))),
       direction: DismissDirection.startToEnd,
       child: CheckboxListTile(
-          title: Text(_toDoList[index]['name']),
+          title: Text(_toDoList[index]['txtTarefa']),
           value: _toDoList[index]["ok"],
           secondary: CircleAvatar(
               child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error)),
@@ -159,7 +199,7 @@ class _TasksPageState extends State<TasksPage> {
           _saveData();
 
           final snack = SnackBar(
-            content: Text("Tarefa ${_lastRemoved['name']} removida"),
+            content: Text("Tarefa ${_lastRemoved['nmCateg']} removida"),
             action: SnackBarAction(
               label: "Desfazer",
               onPressed: () {
@@ -186,7 +226,7 @@ class _TasksPageState extends State<TasksPage> {
   Future<File> _saveData() async {
     var newMyList = _toDoList + _toDoList2;
     _toDoList2 = [];
-    String data = json.encode(newMyList);   //  json.encode(_toDoList2);
+    String data = json.encode(newMyList); //  json.encode(_toDoList2);
     final file = await _getFile();
     return file.writeAsString(data);
   }
